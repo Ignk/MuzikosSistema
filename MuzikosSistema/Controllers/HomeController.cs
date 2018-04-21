4a26f4d4d095eff6d59261bed32773af93bc1ff0
Ignas Karazija <karazijaignas@gmail.com>
2018-04-21 20:43:52 +0300

    Data model

--- MuzikosSistema/Controllers/HomeController.cs
+++ MuzikosSistema/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MuzikosSistema.Models;
 
 namespace MuzikosSistema.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext context = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
